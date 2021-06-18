486866ea5f530a0942f2ede609ca34f3d65ff34a
Dejan Sikirica <sikiricadejan@gmail.com>
2021-06-18 12:11:28 +0200

    floor initalzed

--- Assets/Scripts/Level/LevelData.cs
+++ Assets/Scripts/Level/LevelData.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class LevelData : MonoBehaviour
 {
-    public int SizeX { get; set; } = 25;
-    public int SizeY { get; set; } = 20;
+    private const int minSizeX = 6;
+    private const int minSizeY = 6;
+    private const int maxSizeX = 666;
+    private const int maxSizeY = 666;
+    private int sizeX = 25;
+    private int sizeY = 20;
 
+    public int SizeX
+    {
+        get => sizeX;
+        set
+        {
+            if (value >= minSizeX && value <= maxSizeX) sizeX = value;
+        }
+    }
+    public int SizeY
+    {
+        get => sizeY;
+        set
+        {
+            if (value >= minSizeY && value <= maxSizeY) sizeY = value;
+        }
+    }
 
 
 }
--- Assets/Scripts/Level/LevelInitalizer.cs
+++ Assets/Scripts/Level/LevelInitalizer.cs
@@ -7,12 +7,14 @@ using UnityEngine.Tilemaps;
 public class LevelInitalizer : MonoBehaviour
 {
     public LevelData levelData;
-    public Tilemap floor;
+    public Tilemap FloorTileMap;
+    public TileBase FloorTileSample1;
+    public TileBase ObstacleSample1;
 
-    void OnAwake()
+    void Awake()
     {
+        levelData = new LevelData();
         InitalizeTiles();
-
     }
 
     private void InitalizeTiles()
@@ -23,6 +25,11 @@ public class LevelInitalizer : MonoBehaviour
 
     private void InitlaizeFloorAndBoundries()
     {
-        
+        for (int i = 0; i < levelData.SizeX; i++)
+            for (int j = 0; j < levelData.SizeY; j++)
+            {
+                FloorTileMap.SetTile(new Vector3Int(i, j, 0), FloorTileSample1);
+            }
+
     }
 }
