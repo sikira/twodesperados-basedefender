5ad81b53793a743abf7a2cb1df5e53e4bbdabee2
Dejan Sikirica <sikiricadejan@gmail.com>
2021-06-18 13:01:39 +0200

    player spawned

--- Assets/Scripts/Level/LevelData.cs
+++ Assets/Scripts/Level/LevelData.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class LevelData
 {
-    private const int minSizeX = 6;
-    private const int minSizeY = 6;
+    private const int minSizeX = 10;
+    private const int minSizeY = 10;
     private const int maxSizeX = 666;
     private const int maxSizeY = 666;
     private int sizeX = 25;
@@ -25,6 +26,29 @@ public class LevelData
         set
         {
             if (value >= minSizeY && value <= maxSizeY) sizeY = value;
+        }
+    }
+
+    public RectInt BaseArea
+    {
+        get
+        {
+            var centerPoint = new Point(SizeX / 2, SizeY / 2);
+            //TODO: make offset array 1/5 of map or 15 max;
+
+            var minAreaSize = Mathf.Min(SizeX, SizeY);
+
+            var offsetArea = minAreaSize / 5 > 15 ? 15 : Mathf.FloorToInt(minAreaSize / 5);
+            if (offsetArea < 2)
+                offsetArea = 2;
+
+            return new RectInt(centerPoint.X - offsetArea, centerPoint.X + offsetArea, centerPoint.Y - offsetArea, centerPoint.Y + offsetArea);
+            // return new Vector3Int[]{
+            //     new Vector3Int(centerPoint.X - offsetArea, centerPoint.Y - offsetArea,0),
+            //     new Vector3Int(centerPoint.X + offsetArea, centerPoint.Y - offsetArea,0),
+            //     new Vector3Int(centerPoint.X + offsetArea, centerPoint.Y + offsetArea,0),
+            //     new Vector3Int(centerPoint.X - offsetArea, centerPoint.Y + offsetArea,0)
+            // };
         }
     }
 
--- Assets/Scripts/Level/LevelInitalizer.cs
+++ Assets/Scripts/Level/LevelInitalizer.cs
@@ -6,10 +6,14 @@ using UnityEngine.Tilemaps;
 
 public class LevelInitalizer : MonoBehaviour
 {
+    public Camera mainCamera;
     public LevelData levelData;
     public Tilemap FloorTileMap;
+    public Tilemap ObstacleTileMap;
+    public Tilemap PlayerTileMap;
     public TileBase FloorTileSample1;
     public TileBase ObstacleSample1;
+    public TileBase PlayerSample1;
 
     void Awake()
     {
@@ -20,25 +24,31 @@ public class LevelInitalizer : MonoBehaviour
     private void InitalizeTiles()
     {
         InitlaizeFloorAndBoundries();
+        InitalizePlayer();
+    }
+
+    private void InitalizePlayer()
+    {
+        var baseArea = levelData.BaseArea;
+        var playerStartPosition = new Vector3Int(
+            UnityEngine.Random.Range(baseArea.x, baseArea.y),
+            UnityEngine.Random.Range(baseArea.width, baseArea.height), 0
+        );
+        // set player position
+        PlayerTileMap.SetTile(playerStartPosition, PlayerSample1);
+        // set camera position
+        mainCamera.transform.position = playerStartPosition + new Vector3Int(0, 0, -30);
+
+
     }
 
     private void InitlaizeFloorAndBoundries()
     {
         for (int i = 0; i < levelData.SizeX; i++)
             for (int j = 0; j < levelData.SizeY; j++)
-            {
                 if (i == 0 || j == 0 || i == levelData.SizeX - 1 || j == levelData.SizeY - 1)
-                {
                     FloorTileMap.SetTile(new Vector3Int(i, j, 0), ObstacleSample1);
-                }
                 else
-                {
                     FloorTileMap.SetTile(new Vector3Int(i, j, 0), FloorTileSample1);
-                }
-
-            }
-
-
-
     }
 }
