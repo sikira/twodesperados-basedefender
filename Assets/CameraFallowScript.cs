0b6ba3cc7823b546c5dc8c230cd195c2132da190
Dejan Sikirica <sikiricadejan@gmail.com>
2021-06-18 15:20:34 +0200

    setting player and base as prefab

---
+++ Assets/CameraFallowScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFallowScript : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null)
+        {
+            target = new GameObject();
+            // throw new NullReferenceException("Target must def");
+        }
+        Update();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // set camera position
+        this.transform.position = target.transform.position + new Vector3Int(0, 0, -30);
+    }
+}
--- Assets/Scripts/Level/LevelInitalizer.cs
+++ Assets/Scripts/Level/LevelInitalizer.cs
@@ -7,11 +7,13 @@ using UnityEngine.Tilemaps;
 
 public class LevelInitalizer : MonoBehaviour
 {
+    public MainPlayer player;
+    public PlayerBase playerBase;
     public Camera mainCamera;
+
     public LevelData levelData;
     public Tilemap FloorTileMap;
     public Tilemap ObstacleTileMap;
-    public Tilemap PlayerTileMap;
     public Tilemap EnemyTileMap;
     public TileBase FloorTileSample1;
     public TileBase ObstacleSample1;
@@ -20,7 +22,7 @@ public class LevelInitalizer : MonoBehaviour
     public TileBase SpawnerTileMap;
 
 
-    private List<Vector3Int> posibleSpawnListPosition = new();
+    private List<Vector3Int> spawnListPosition = new List<Vector3Int>();
 
     void Awake()
     {
@@ -38,19 +40,42 @@ public class LevelInitalizer : MonoBehaviour
     private void InitalizeEnemySpawner()
     {
         var random = new System.Random();
+        // **** Solution 1  ****//
         var spawnerPosition = new List<Vector3Int>();
-        for (int i = 0; i < levelData.NumberOfEnemySpawner; i++)
+        for (int i = 0; i < levelData.NumberOfEnemySpawner && spawnListPosition.Count() > 0; i++)
         {
-            var pos = random.Next(0, posibleSpawnListPosition.Count());
-            spawnerPosition.Add(posibleSpawnListPosition[pos]);
-            posibleSpawnListPosition.RemoveAt(pos);
+            // better random resoults, there other ways to create this. Use seed :D
+            for (int j = 0; j < 50; j++)
+                random.Next(0, 100);
+
+            var pos = random.Next(0, spawnListPosition.Count());
+            if (pos > spawnerPosition.Count())
+            {
+                spawnerPosition.Add(spawnListPosition[pos]);
+                spawnListPosition.RemoveAt(pos);
+            }
         }
         foreach (var pos in spawnerPosition)
-        {
             EnemyTileMap.SetTile(pos, SpawnerTileMap);
-        }
 
+        // **** Solution 2  ****//
+        // while (spawnListPosition.Count() > levelData.NumberOfEnemySpawner)
+        //     spawnListPosition.RemoveAt(random.Next(0, spawnListPosition.Count()));
+        // foreach (var pos in spawnListPosition)
+        //     EnemyTileMap.SetTile(pos, SpawnerTileMap);
 
+        // **** Solution 3  ****//
+        // var spawnerPosition = new List<Vector3Int>();
+        // var stepPosition = 0;
+        // for (int i = 0; i < levelData.NumberOfEnemySpawner && spawnListPosition.Count() > 0; i++)
+        // {
+        //     stepPosition += random.Next(5, spawnListPosition.Count() + 7);
+        //     var pos = stepPosition % spawnListPosition.Count();
+        //     spawnerPosition.Add(spawnListPosition[pos]);
+        //     spawnListPosition.RemoveAt(pos);
+        // }
+        // foreach (var pos in spawnerPosition)
+        //     EnemyTileMap.SetTile(pos, SpawnerTileMap);
 
 
     }
@@ -63,15 +88,16 @@ public class LevelInitalizer : MonoBehaviour
 
         var playerStartPosition = getRandomVector3();
         // set player position
-        PlayerTileMap.SetTile(playerStartPosition, PlayerSample1);
-        // set camera position
-        mainCamera.transform.position = playerStartPosition + new Vector3Int(0, 0, -30);
+        player.GetComponent<Rigidbody2D>().MovePosition(new Vector2(playerStartPosition.x, playerStartPosition.y));
+        
 
         // spawn base
         Vector3Int basePosition = playerStartPosition;
         while (basePosition == playerStartPosition)
             basePosition = getRandomVector3();
-        PlayerTileMap.SetTile(basePosition, BaseTileMap);
+
+        playerBase.GetComponent<Rigidbody2D>().MovePosition(new Vector2(basePosition.x, basePosition.y));
+        // playerBase.transform.position = basePosition;
 
     }
 
@@ -80,13 +106,14 @@ public class LevelInitalizer : MonoBehaviour
         for (int i = 0; i < levelData.SizeX; i++)
             for (int j = 0; j < levelData.SizeY; j++)
             {
-                if (i == 1 || j == 1 || i == levelData.SizeX - 2 || j == levelData.SizeY - 2)
-                    posibleSpawnListPosition.Add(new Vector3Int(i, j, 0));
-
                 if (i == 0 || j == 0 || i == levelData.SizeX - 1 || j == levelData.SizeY - 1)
                     FloorTileMap.SetTile(new Vector3Int(i, j, 0), ObstacleSample1);
                 else
+                {
+                    if (i == 1 || j == 1 || i == levelData.SizeX - 2 || j == levelData.SizeY - 2)
+                        spawnListPosition.Add(new Vector3Int(i, j, 0));
                     FloorTileMap.SetTile(new Vector3Int(i, j, 0), FloorTileSample1);
+                }
             }
     }
 }
