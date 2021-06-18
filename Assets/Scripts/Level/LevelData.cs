ad404abab20b8bf22c54bdb1e01d5ec73f699720
Dejan Sikirica <sikiricadejan@gmail.com>
2021-06-18 11:56:03 +0200

    lets start

---
+++ Assets/Scripts/Level/LevelData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    public int SizeX { get; set; } = 25;
+    public int SizeY { get; set; } = 20;
+
+
+
+}
---
+++ Assets/Scripts/Level/LevelInitalizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LevelInitalizer : MonoBehaviour
+{
+    public LevelData levelData;
+    public Tilemap floor;
+
+    void OnAwake()
+    {
+        InitalizeTiles();
+
+    }
+
+    private void InitalizeTiles()
+    {
+        InitlaizeFloorAndBoundries();
+
+    }
+
+    private void InitlaizeFloorAndBoundries()
+    {
+        
+    }
+}
