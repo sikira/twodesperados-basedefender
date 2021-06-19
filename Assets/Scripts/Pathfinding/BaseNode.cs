67036a759ba3480585e63c3e2d2eb843b963530b
Dejan Sikirica <sikiricadejan@gmail.com>
2021-06-19 09:26:16 +0200

    Pocnjem Astar Algoritam

---
+++ Assets/Scripts/Pathfinding/BaseNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseNode : MonoBehaviour
+{
+    // Vrijednost od pocetka
+    public int G = 0;
+    // Pretpostavljena Vrijednost do cilja
+    public int H = 0;    
+    // Ukupna cijena 
+    public int F => H + G;
+
+    
+}
