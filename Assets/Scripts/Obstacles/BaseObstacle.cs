6af7a7c15e5e07cb44bb100aa4b1abc36c7ad77b
Dejan Sikirica <sikiricadejan@gmail.com>
2021-06-23 02:13:36 +0200

    dodaojem animaciju

--- Assets/Scripts/Obstacles/BaseObstacle.cs
+++ Assets/Scripts/Obstacles/BaseObstacle.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 // public class BaseObstacle : MonoBehaviour
-public class BaseObstacle : MonoBehaviour
+public class BaseObstacle : MonoBehaviour, IHittableObject
 {
-
     public Vector2Int Position;
 
     public BaseObstacle(Vector2Int position)
     {
         Position = position;
+    }
+
+    private int _health = 150;
+    public int Health => _health;
+
+    public void HealMe(int power)
+    {
+        _health += power;
+    }
+
+    public void HitMe(int power)
+    {
+        _health -= power;
+        if (_health <= 0)
+        {
+            DestroyMe();
+        }
+    }
+
+    private void DestroyMe()
+    {
+        this.gameObject.SetActive(false);
+        
+        //TODO: Obavjesti sve da se promjenila fizika
+
     }
 }
